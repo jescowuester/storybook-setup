9648a6dbc35c5178bb532688f7b7b1e6b97408cf
jesco <jesco@storyofams.com>
2019-05-30 10:49:12 +0200

    headers

--- pages/_document.js
+++ pages/_document.js
@@ -27,11 +27,33 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+
   render() {
     return (
       <Html>
         <Head>
-          <style>{`body { margin: 0 } /* custom! */`}</style>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon/favicon-16x16.png"
+          />
+          <link
+            rel="mask-icon"
+            href="/static/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
         </Head>
         <body className="custom_class">
           <Main />
